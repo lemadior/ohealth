dc4523d60febc35424d63f463f25f3d7ad67161b
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2026-07-06 11:31:49 +0300

    #285 add barcode generator, composer update

--- resources/views/livewire/person/records/partials/information-certificate.blade.php
+++ resources/views/livewire/person/records/partials/information-certificate.blade.php
@@ -1,3 +1,6 @@
+@use('Picqer\Barcode\Types\TypeCode128A')
+@use('Picqer\Barcode\Renderers\DynamicHtmlRenderer')
+
 <div
     x-show="showCertificate"
     style="display:none"
@@ -18,68 +21,25 @@
                     {{ __('preperson.info_certificate') }}
                 </h2>
 
-                <p class="text-sm text-gray-600 dark:text-gray-400 mb-1">{{ __('preperson.label_single') }}</p>
-                <p class="text-sm font-mono text-gray-900 dark:text-white mb-5 uppercase tracking-wide">
-                    {{ $preperson->uuid }}
-                </p>
+                <p class="text-sm text-gray-600 dark:text-gray-400 mb-4">{{ __('preperson.label_single') }}</p>
 
-                {{-- todo:use JS library --}}
-                <div class="flex items-center justify-center mb-6">
-                    @php
-                        $patterns = [
-                            '212222', '222122', '222221', '121223', '121322', '131222', '122213', '122312', '132212', '221213',
-                            '221312', '231212', '112232', '122132', '122231', '113222', '123122', '123221', '223211', '221132',
-                            '221231', '213212', '223112', '312131', '311222', '321122', '321221', '312212', '322112', '322211',
-                            '212123', '212321', '232121', '111323', '131123', '131321', '112313', '132113', '132311', '211313',
-                            '231113', '231311', '112133', '112331', '132131', '113123', '113321', '133121', '313121', '211331',
-                            '231131', '213113', '213311', '213131', '311123', '311321', '331121', '312113', '312311', '332111',
-                            '314111', '221411', '431111', '111224', '111422', '121124', '121421', '141122', '141221', '112214',
-                            '112412', '122114', '122411', '142112', '142211', '241211', '221114', '413111', '241112', '134111',
-                            '111242', '121142', '121241', '114212', '124112', '124211', '411212', '421112', '421211', '212141',
-                            '214121', '412121', '111143', '111341', '131141', '114113',
-                            '114311', '411113', '411311', '113141', '114131', '311141', '411131',
-                            '211412', '211214', '211232',
-                            '2331112'
-                        ];
+                @php
+                    $barcodeValue = strtoupper($preperson->uuid);
+                    $barcode = new TypeCode128A()->getBarcode($barcodeValue);
+                    $barcodeHtml = new DynamicHtmlRenderer()->render($barcode);
+                @endphp
 
-                        $encodedValues = [103];
-                        $checksum = 103;
-
-                        $len = strlen($preperson->uuid);
-                        for ($i = 0; $i < $len; $i++) {
-                            $char = $preperson->uuid[$i];
-                            $val = ord($char) - 32;
-                            if ($val < 0 || $val > 95) {
-                                $val = 0;
-                            }
-                            $encodedValues[] = $val;
-                            $checksum += ($i + 1) * $val;
-                        }
-
-                        $checkDigit = $checksum % 103;
-                        $encodedValues[] = $checkDigit;
-                        $encodedValues[] = 106;
-
-                        $svgContent = '';
-                        $x = 0;
-                        foreach ($encodedValues as $val) {
-                            $pattern = $patterns[$val];
-                            $patLen = strlen($pattern);
-                            for ($p = 0; $p < $patLen; $p++) {
-                                $width = (int)$pattern[$p];
-                                $isBar = ($p % 2 === 0);
-                                if ($isBar) {
-                                    $svgContent .= '<rect x="' . $x . '" y="0" width="' . $width . '" height="80" fill="#000"/>';
-                                }
-                                $x += $width;
-                            }
-                        }
-                    @endphp
-                    <svg viewBox="0 0 {{ $x }} 80" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg"
-                         class="w-full"
-                         style="width: 100%; height: 2cm; min-width: 9cm; min-height: 1.2cm; display: block; margin: 0 auto;">
-                        {!! $svgContent !!}
-                    </svg>
+                <div class="flex flex-col items-center gap-2 mb-6">
+                    <div class="w-full bg-white p-2 rounded">
+                        <div
+                            style="width:100%;min-width:9cm;height:1.2cm;print-color-adjust:exact;-webkit-print-color-adjust:exact"
+                        >
+                            {!! $barcodeHtml !!}
+                        </div>
+                    </div>
+                    <span class="text-xs font-mono uppercase tracking-widest text-gray-900 dark:text-white">
+                        {{ $barcodeValue }}
+                    </span>
                 </div>
 
                 <h3 class="text-base font-bold text-gray-900 dark:text-white mb-3">{{ __('patients.main_info') }}</h3>
